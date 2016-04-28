71d6c62201fa05fa13bcc42643a8ff024622148d
Du Cheng <ducheng2@gmail.com>
2016-04-29 02:46:30 +0800

    finished

--- web.sh
+++ web.sh
@@ -8,7 +8,7 @@ declare -A headers
 while /bin/true; do
     read header
     [ "$header" == $'\r' ] && break;
-    values=(${header//:/})
+    IFS=":" read -a values <<< $header
     headers[${values[0]}]="${values[@]:1}"
 done
 
@@ -61,6 +61,11 @@ function get_session_cache() {
     sessionid=$(echo $1 | tr '\r' ' ')
     [ -n "$sessionid" ] && grep "$sessionid" /tmp/entry.cache
 }
+function invalidate_cache() {
+    sessionid=$(echo $1 | tr '\r' ' ')
+    sed -e "/^$sessionid/d" -i /tmp/entry.cache
+
+}
 
 function login() {
     if [ "$method" == "post" ]
@@ -98,53 +103,92 @@ function login() {
     fi
 }
 
-function upload() {
-    upload_base=$base/static/images
-    if [ $method == "get" ]; then
-        echo -e "HTTP/1.1 405 Method Not Allowed\r"
-        echo -e "\r"
-    else
-        echo -e "HTTP/1.1 100 Continue\r"
-        echo -e "\r"
-        content="${headers['Content-Type']}"
-        length="${headers['Content-Length']}"
-        boundary=${content#multipart/form-data; boundary=}
-        separator=--$boundary
-        end_separator=$separator--
-        sofar=0
-        length=$(echo $length | tr '\r' ' ')
-        left=$length
-
-        while true
-        do
-            read line
-            left=$(expr $left \- ${#line} \- 1)
-            [ "$line" == $'\r' ] && break
-        done
-
-        uuid=$(uuidgen)
-
-        upload_file_size=$(expr $left \- ${#end_separator} \- 3)
-        head -c $upload_file_size > $upload_base/$uuid
-        head -c $(expr ${#end_separator} + 3) > /dev/null
-
-        printf "HTTP/1.1 200 OK\r\n"
-        printf "Content-Type: text/plain\r\n"
-        printf "Content-Length: ${#uuid}\r\n"
-        printf "\r\n"
-        printf $uuid
-    fi
-}
-
-function home() {
+function logout_() {
     sessionid=${cookies[sessionid]}
     session=$(get_session_cache "$sessionid")
+    if [ -n "$session" ]
+    then
+        invalidate_cache $sessionid
+    fi
+    echo -e "HTTP/1.1 302 Found\r"
+    echo -e "Location: /\r"
+    echo -e "\r"
+}
+
+function upload() {
+    sessionid=${cookies[sessionid]}
+    session=$(get_session_cache "$sessionid")
+    read -a list <<< $session
+    username=${list[1]}
+    upload_base=$base/static/profile
     if [ -z "$session" ]; then
         echo -e "HTTP/1.1 302 Found\r"
         echo -e "Location: /login\r"
         echo -e "\r"
     else
-        static /templates/index.html
+        if [ $method == "get" ]; then
+            echo -e "HTTP/1.1 405 Method Not Allowed\r"
+            echo -e "\r"
+        else
+            echo -e "HTTP/1.1 100 Continue\r"
+            echo -e "\r"
+            content="${headers['Content-Type']}"
+            length="${headers['Content-Length']}"
+            boundary=${content#multipart/form-data; boundary=}
+            separator=--$boundary
+            end_separator=$separator--
+            sofar=0
+            length=$(echo $length | tr '\r' ' ')
+            left=$length
+
+            while true
+            do
+                read line
+                left=$(expr $left \- ${#line} \- 1)
+                [ "$line" == $'\r' ] && break
+            done
+
+            uuid=$(uuidgen)
+
+            upload_file_size=$(expr $left \- ${#end_separator} \- 3)
+            head -c $upload_file_size > $upload_base/$uuid
+            head -c $(expr ${#end_separator} + 3) > /dev/null
+            # update db
+            echo "update users set profile=\"$uuid\" where username=\"$username\"" | mysql -u entry_user -h 203.117.172.31 -pentry_password -D entry_task 2>/dev/null
+            # purge
+            invalidate_cache $sessionid
+            result=$(echo "select username, profile from users where username=\"$username\"" | mysql -u entry_user -h 203.117.172.31 -pentry_password -D entry_task 2>/dev/null | tail -n 1)
+            session_key=$(echo $sessionid | tr '\r' ' ')
+            set_session_cache $session_key $result
+
+            printf "HTTP/1.1 302 Found\r\n"
+            printf "Content-Type: text/plain\r\n"
+            printf "Location: /\r\n"
+            printf "\r\n"
+        fi
+    fi
+}
+
+
+function home() {
+    sessionid=${cookies[sessionid]}
+    session=$(get_session_cache "$sessionid")
+    read -a list <<< $session
+    username=${list[1]}
+    image=${list[2]}
+    if [ "$image" = "NULL" ];
+    then
+        image='default.png'
+    fi
+    if [ -z "$session" ]; then
+        echo -e "HTTP/1.1 302 Found\r"
+        echo -e "Location: /login\r"
+        echo -e "\r"
+    else
+        echo -e "HTTP/1.1 200 OK\r"
+        echo -e "Content-Type: text/html\r"
+        echo -e "\r"
+        sed -e "s/USERNAME/${username}/g" -e "s/PROFILEIMG/${image}/g" $base/templates/index.html
     fi
 }
 
@@ -155,6 +199,8 @@ function application() {
             home ;;
         "/login")
             login ;;
+        "/logout")
+            logout_ ;;
         "/upload")
             upload ;;
     esac
